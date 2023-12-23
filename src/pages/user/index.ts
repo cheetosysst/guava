b9de729291e7ccb5824dbd4abba77c3c9a5cde46
cheetosysst <thect@protonmail.com>
2023-12-23 10:45:04 +0800

    feat: signin & signup page

--- src/pages/user/index.ts
+++ src/pages/user/index.ts
@@ -1,9 +1,11 @@
 import Elysia from "elysia";
 import SignUpPage from "./signup";
 import IndexPage from "./indexPage";
+import SignInPage from "./signin";
 
 const userRoute = new Elysia({ prefix: "/user" })
 	.use(IndexPage)
+	.use(SignInPage)
 	.use(SignUpPage);
 
 export default userRoute;
--- src/pages/user/signin.tsx
+++ src/pages/user/signin.tsx
@@ -1,24 +1,22 @@
 import Elysia from "elysia";
 import MainLayout from "../../layouts/main";
 
-const Page = new Elysia().get("/signup", () => (
-	<MainLayout title="Signup">
+const Page = new Elysia().get("/signin", () => (
+	<MainLayout title="Signin">
 		<div
 			class="hero h-screen shadow-md bg-base-100"
 			style="background-image: url(https://picsum.photos/id/1081/1000/800);"
 		>
 			<div class="hero-overlay bg-opacity-80" />
-			<div class="hero-content text-white px-10 drop-shadow-md flex-col lg:flex-row-reverse">
-				<div class="text-justify lg:text-left">
-					<h1 class="text-center text-5xl font-bold text-warning">
-						Singup now!
-					</h1>
+			<div class="hero-content text-white px-10 drop-shadow-md flex-col-reverse lg:flex-row-reverse">
+				<div class="text-center lg:text-left">
+					<h1 class="text-5xl font-bold text-warning">Singup now!</h1>
 					<p class="py-6">
-						Unleash the power of savings! Join ShopSmart Rewards now
-						and experience a world where every purchase brings you
-						closer to exclusive deals, cash back rewards, and
-						personalized shopping joy. Sign up for free and elevate
-						your shopping journey today!
+						Welcome back to ShopSmart Rewards! Sign in to your
+						account and rediscover the joy of seamless shopping,
+						personalized recommendations, and exclusive savings.
+						Your shopping adventure continues here – let's dive in
+						together!
 					</p>
 				</div>
 				<div class="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
@@ -30,17 +28,6 @@ const Page = new Elysia().get("/signup", () => (
 							<input
 								type="text"
 								placeholder="Username"
-								class="input input-bordered"
-								required
-							/>
-						</div>
-						<div class="form-control">
-							<label class="label">
-								<span class="label-text">Email</span>
-							</label>
-							<input
-								type="email"
-								placeholder="Email"
 								class="input input-bordered"
 								required
 							/>
@@ -67,13 +54,9 @@ const Page = new Elysia().get("/signup", () => (
 						<div class="form-control mt-6">
 							<input
 								type="submit"
-								value="Sign Up!"
 								class="btn btn-primary"
+								value="Sing in"
 							/>
-							<div class="divider text-primary-content">OR</div>
-							<a href="/user/signin" class="btn btn-neutral">
-								Member login
-							</a>
 						</div>
 					</form>
 				</div>
